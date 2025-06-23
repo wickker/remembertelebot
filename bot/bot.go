ca8309f84c87a905e2699be785dd7de2fa7b90dd
Dian Chun Choy <choydianchun@gmail.com>
2025-06-23 17:40:28 +0800

    change implementation of tele msg processing from polling to webhook

--- bot/bot.go
+++ bot/bot.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Client struct {
-	bot *tgbotapi.BotAPI
+	Bot *tgbotapi.BotAPI
 }
 
 func NewClient(envCfg config.EnvConfig) (*Client, error) {
@@ -17,21 +17,31 @@ func NewClient(envCfg config.EnvConfig) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	bot.Debug = envCfg.IsDev()
+
+	webhook, err := tgbotapi.NewWebhook(fmt.Sprintf("%s/webhooks", envCfg.BaseURL))
+	if err != nil {
+		return nil, err
+	}
+	if _, err := bot.Request(webhook); err != nil {
+		return nil, err
+	}
 
 	return &Client{
-		bot: bot,
+		Bot: bot,
 	}, nil
 }
 
-func (c *Client) CreateBotChannel() tgbotapi.UpdatesChannel {
-	cfg := tgbotapi.NewUpdate(0)
-	cfg.Timeout = 60
-	return c.bot.GetUpdatesChan(cfg)
-}
+// Old implementation for reference:
+//func (c *Client) CreateBotChannel() tgbotapi.UpdatesChannel {
+//	cfg := tgbotapi.NewUpdate(0)
+//	cfg.Timeout = 60
+//	return c.Bot.GetUpdatesChan(cfg)
+//}
 
 func (c *Client) SendPlainMessage(chatID int64, message string) error {
 	msg := tgbotapi.NewMessage(chatID, message)
-	if _, err := c.bot.Send(msg); err != nil {
+	if _, err := c.Bot.Send(msg); err != nil {
 		return fmt.Errorf("bot failed to send plain message [messageConfig: %+v]: %w", msg, err)
 	}
 	return nil
@@ -39,7 +49,7 @@ func (c *Client) SendPlainMessage(chatID int64, message string) error {
 
 func (c *Client) SendCallbackConfig(queryID, text string) error {
 	callbackCfg := tgbotapi.NewCallback(queryID, text)
-	if _, err := c.bot.Send(callbackCfg); err != nil {
+	if _, err := c.Bot.Send(callbackCfg); err != nil {
 		return fmt.Errorf("bot failed to send callback config [callbackConfig: %+v]: %w", callbackCfg, err)
 	}
 	return nil
@@ -50,7 +60,7 @@ func (c *Client) SendHtmlMessage(chatID int64, text string, markup interface{}) 
 	msg.ParseMode = tgbotapi.ModeHTML
 	msg.ReplyMarkup = markup
 
-	if _, err := c.bot.Send(msg); err != nil {
+	if _, err := c.Bot.Send(msg); err != nil {
 		return fmt.Errorf("bot failed to send html message [messageConfig: %+v]: %w", msg, err)
 	}
 	return nil
@@ -59,7 +69,7 @@ func (c *Client) SendHtmlMessage(chatID int64, text string, markup interface{}) 
 func (c *Client) SendEditMessage(chatID int64, messageID int, text string) error {
 	msg := tgbotapi.NewEditMessageText(chatID, messageID, text)
 
-	if _, err := c.bot.Send(msg); err != nil {
+	if _, err := c.Bot.Send(msg); err != nil {
 		return fmt.Errorf("bot failed to send edit message [messageConfig: %+v]: %w", msg, err)
 	}
 	return nil
--- config/config.go
+++ config/config.go
@@ -6,6 +6,7 @@ type EnvConfig struct {
 	Env              string `env:"ENV" envDefault:"dev"`
 	DatabaseURL      string `env:"DATABASE_URL"`
 	TelegramBotToken string `env:"TELEGRAM_BOT_TOKEN"`
+	BaseURL          string `env:"BASE_URL"`
 }
 
 func (c EnvConfig) IsDev() bool {
